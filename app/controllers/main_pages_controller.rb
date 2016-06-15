a987edc6bdd48a17bf2ea9be2915e728755febd5
Sam Thomas <spthomas145@gmail.com>
2016-06-15 21:03:45 +0100

    Change flash from error to danger

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -4,9 +4,9 @@ class MainPagesController < ApplicationController
   	if params['search']
   		@recipes = Recipe.type_search(params['search'])
   		if @recipes.present?
-  			flash.now[:success] = "we found #{@recipes.count} recipes matching your search"
+  			flash.now[:success] = "We found #{@recipes.count} recipes matching your search"
   		else
-  			flash.now[:error] = 'We can\'t find any recipes matching your search please try again'
+  			flash.now[:danger] = 'We can\'t find any recipes matching your search please try again'
   			@recipes = Recipe.all
   		end
   	else
