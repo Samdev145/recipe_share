1b3f64f319d7174ca1f86dde6cf97d379077b204
Sam Thomas <spthomas145@gmail.com>
2016-06-15 20:15:00 +0100

    Return response from search along with flash message

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,10 +1,16 @@
 class MainPagesController < ApplicationController
 
   def home
-  	@recipes = if params['search']
-  		Recipe.type_search(params['search'])
+  	if params['search']
+  		@recipes = Recipe.type_search(params['search'])
+  		if @recipes.present?
+  			flash.now[:success] = "we found #{@recipes.count} recipes matching your search"
+  		else
+  			flash.now[:error] = 'We can\'t find any recipes matching your search please try again'
+  			@recipes = Recipe.all
+  		end
   	else
-  		Recipe.all
+  		@recipes = Recipe.all
   	end
   end
 
--- spec/controllers/main_pages_controller_spec.rb
+++ spec/controllers/main_pages_controller_spec.rb
@@ -27,15 +27,34 @@ RSpec.describe MainPagesController, type: :controller do
     end
 
     context 'when a search is made' do
-      before do
-        create(:recipe, name: 'Sheperds Pie')
-        create(:recipe, name: 'Pie and chips')
-        create(:recipe, name: 'Steak and Ale pie')
-        get :home, search: 'Pie'
+
+      context 'when a search is successful' do
+        before do
+          create(:recipe, name: 'Sheperds Pie')
+          create(:recipe, name: 'Pie and chips')
+          create(:recipe, name: 'Steak and Ale pie')
+          get :home, search: 'Pie'
+        end
+
+        it 'should retieve the correct number of recipes' do
+          expect(assigns(:recipes).count).to eq(3)
+        end
+
+        it 'should create the correct flash message' do
+          expect(flash.now[:success]).to eq("we found 3 recipes matching your search")
+        end
       end
 
-      it 'should retieve the correct number of recipes' do
-        expect(assigns(:recipes).count).to eq(3)
+      context 'when the search fails' do
+        before { get :home, search: 'Pie' }
+
+        it 'should set a flash message' do
+          expect(flash.now[:error]).to match(/We can't find any recipes matching your search please try again/)
+        end
+
+        it 'should return all recipes' do
+          expect(assigns(:recipes).count).to eq(Recipe.all.count)
+        end
       end
     end
   end
